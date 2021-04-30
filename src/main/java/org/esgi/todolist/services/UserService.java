3267f697f7375fc9b54f30f9948a224879f11bd4
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-30 14:55:46 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/org/esgi/todolist/services/UserService.java
+++ src/main/java/org/esgi/todolist/services/UserService.java
@@ -1,9 +1,28 @@
 package org.esgi.todolist.services;
 
+import org.esgi.todolist.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class UserService {
+
+
+    public boolean isValid(User user) {
+        return isValidEmail(user.getEmail())
+                && StringUtils.hasText(user.getFirstname())
+                && StringUtils.hasText(user.getLastname())
+    }
+
+    private boolean isValidEmail(String email) {
+        return email.matches("^(.+)@(.+)$");
+    }
+
+    private User createTodolist(User user) {
+        return null;
+    }
+
+    private User 
 
 }
