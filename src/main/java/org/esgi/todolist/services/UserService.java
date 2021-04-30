c9741e4103fbbc49b0dc550dd565720ed8b03092
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-30 15:02:31 +0200

    add more methods

--- src/main/java/org/esgi/todolist/services/UserService.java
+++ src/main/java/org/esgi/todolist/services/UserService.java
@@ -1,5 +1,6 @@
 package org.esgi.todolist.services;
 
+import org.esgi.todolist.models.Item;
 import org.esgi.todolist.models.User;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -22,8 +23,17 @@ public class UserService {
         return null;
     }
 
-    private User addItem(User user) {
+    private User addItem(User user, Item item) {
         return null;
     }
+
+    private User removeItem(User user, int itemIndex) {
+        return null;
+    }
+
+    private User updateItem(User user, Item item, int itemIndex) {
+        return null;
+    }
+
 
 }
