41d8124927c53c5e49d5605bee0d1b7a2bf95054
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-30 15:37:53 +0200

    fill services

--- src/main/java/org/esgi/todolist/services/TodoListService.java
+++ src/main/java/org/esgi/todolist/services/TodoListService.java
@@ -6,7 +6,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TodoListService {
+
     public void addItem(ToDoList todolist, Item item) {
 
+    }
+
+    public void removeItem(ToDoList todolist, int itemIndex) {
+
+    }
+
+    public void updateItem(ToDoList todolist, int itemIndex, Item item) {
+
+    }
+
+    public boolean isItemValid(Item item) {
+        return true;
     }
 }
