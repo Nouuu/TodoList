09c8e9436e3ae2803c6db1db40eab301e33d8f67
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-30 15:13:07 +0200

    fill user service

---
+++ src/main/java/org/esgi/todolist/commons/exceptions/UserException.java
@@ -0,0 +1,7 @@
+package org.esgi.todolist.commons.exceptions;
+
+public class UserException extends RuntimeException {
+    public UserException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/esgi/todolist/services/UserService.java
+++ src/main/java/org/esgi/todolist/services/UserService.java
@@ -1,5 +1,6 @@
 package org.esgi.todolist.services;
 
+import org.esgi.todolist.commons.exceptions.UserException;
 import org.esgi.todolist.models.Item;
 import org.esgi.todolist.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +20,33 @@ public class UserService {
     public boolean isValid(User user) {
         return isValidEmail(user.getEmail())
                 && StringUtils.hasText(user.getFirstname())
-                && StringUtils.hasText(user.getLastname());
+                && StringUtils.hasText(user.getLastname())
+                && StringUtils.hasLength(user.getPassword())
+                && user.getPassword().length() >= 8
+                && user.getPassword().length() < 40;
     }
 
     private boolean isValidEmail(String email) {
         return email.matches("^(.+)@(.+)$");
     }
 
-    private User createTodolist(User user) {
+    public User createTodolist(User user) {
+        if (!this.isValid(user)) {
+            throw new UserException("User is not valid");
+        }
+        user.createTodolist();
+        return user;
+    }
+
+    public User addItem(User user, Item item) {
         return null;
     }
 
-    private User addItem(User user, Item item) {
+    public User removeItem(User user, int itemIndex) {
         return null;
     }
 
-    private User removeItem(User user, int itemIndex) {
-        return null;
-    }
-
-    private User updateItem(User user, Item item, int itemIndex) {
+    public User updateItem(User user, Item item, int itemIndex) {
         return null;
     }
 
