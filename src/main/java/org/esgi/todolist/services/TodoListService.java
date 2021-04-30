e95ea46ab321c8151fa8942bda0089ffe70bf43c
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-30 14:48:55 +0200

    create services

---
+++ src/main/java/org/esgi/todolist/services/TodoListService.java
@@ -0,0 +1,7 @@
+package org.esgi.todolist.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoListService {
+}
---
+++ src/main/java/org/esgi/todolist/services/UserService.java
@@ -0,0 +1,9 @@
+package org.esgi.todolist.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
