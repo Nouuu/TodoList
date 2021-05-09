dfc62f62e7368bc809cd3fc5a20c2f3a96034bb7
Clément BOSSARD <cbossard3@myges.fr>
2021-05-09 10:19:44 +0200

    first route test

---
+++ src/test/java/org/esgi/todolist/routes/UserApiTest.java
@@ -0,0 +1,52 @@
+package org.esgi.todolist.routes;
+
+
+import org.esgi.todolist.MvcHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserApiTest {
+
+    @Autowired
+    private UserApi userApi;
+
+    private MvcHelper mvcHelper;
+
+    @BeforeEach
+    void setUp() {
+        mvcHelper = new MvcHelper(userApi, "");
+    }
+
+    @Test
+    void testCreateToDoListWithGoodUser() {
+        String data = "{ \"firstname\": \"firstname\", \"lastname\": \"lastename\", \"email\": \"email@email.email\", \"password\": \"fewfvwouefbw;f\"}";
+        try {
+            mvcHelper.invokePostMethod("ToDoList", data)
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo(data)));
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    void testCreateToDoListWithBadUser() {
+        String data = "{ \"firstname\": \"\", \"lastname\": \"lastename\", \"email\": \"email@email.email\", \"password\": \"fewfvwouefbw;f\"}";
+        try {
+            mvcHelper.invokePostMethod("ToDoList", data)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string(equalTo(data)));
+        } catch (Exception e) {
+
+        }
+    }
+}
