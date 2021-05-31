30f0e92426b51ff7e12b08792778a0ded75ca7eb
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-05-31 17:49:42 +0200

    add repositories

--- src/main/java/org/esgi/todolist/repositories/TodoListRepository.java
+++ src/main/java/org/esgi/todolist/repositories/TodoListRepository.java
@@ -4,9 +4,11 @@ import org.esgi.todolist.models.Item;
 import org.esgi.todolist.models.TodoList;
 import org.esgi.todolist.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Integer> {
     Optional<TodoList> getByUser(User user);
 
