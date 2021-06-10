52b53beacd258042f7588e2d12bf2caa62eadbb3
Nouu <noelarrieulacoste@yahoo.fr>
2021-06-10 17:41:35 +0200

    add tests on toJson

---
+++ src/test/java/org/esgi/todolist/models/UserTest.java
@@ -0,0 +1,26 @@
+package org.esgi.todolist.models;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void toJSON() throws JsonProcessingException {
+        User user = new User("Firstname", "Lastnale", "email@eee.fr", "password");
+        String expected = "{\"firstname\":\"Firstname\",\"lastname\":\"Lastnale\",\"email\":\"email@eee.fr\",\"password\":\"password\",\"id\":0,\"toDoList\":null}";
+        Assertions.assertThat(user.toJSON()).isEqualTo(expected);
+    }
+
+    @Test
+    void toJSONWithEmptyTodoList() throws JsonProcessingException {
+        User user = new User("Firstname", "Lastname", "email@eee.fr", "password");
+        String expected = "{\"firstname\":\"Firstname\",\"lastname\":\"Lastname\",\"email\":\"email@eee.fr\",\"password\":\"password\",\"id\":0,\"toDoList\":{\"id\":0,\"items\":[]}}";
+        user.setToDoList(new TodoList());
+        Assertions.assertThat(user.toJSON()).isEqualTo(expected);
+
+    }
+}
